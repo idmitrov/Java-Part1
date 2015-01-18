849aef8243e8810d48310a6b29aadfd9980f3d30
Dimitar <mailto.dimitrov@gmail.com>
2015-01-18 22:38:57 +0200

    Update DeckOfCards.java

--- Intro-Java-Homework/src/DeckOfCards.java
+++ Intro-Java-Homework/src/DeckOfCards.java
@@ -1,4 +1,4 @@
-﻿import java.io.FileOutputStream;
+import java.io.FileOutputStream;
 import com.itextpdf.text.BaseColor;
 import com.itextpdf.text.Document;
 import com.itextpdf.text.Font;
@@ -12,10 +12,11 @@ public class DeckOfCards {
 		try {
 			// CREATING PDF DOCUMENT
 			Document pdfDoc = new Document();
-			PdfWriter.getInstance(pdfDoc, new FileOutputStream(
-					"Playing-Cards-Deck.pdf"));
+			PdfWriter.getInstance(pdfDoc, new FileOutputStream("Playing-Cards-Deck.pdf"));
+			
 			// OPENING THE PDF
 			pdfDoc.open();
+			
 			// CREATING CARDS BODY
 			PdfPTable cardsBody = new PdfPTable(13);
 			cardsBody.setWidthPercentage(113.4f);
@@ -26,59 +27,42 @@ public class DeckOfCards {
 			cardsBody.getDefaultCell().setBorderWidthTop(0.5f);
 			cardsBody.getDefaultCell().setBorderWidthLeft(0.5f);
 			cardsBody.getDefaultCell().setBorderColor(BaseColor.LIGHT_GRAY);
-			BaseFont fontFamily = BaseFont.createFont("./font/times.ttf",
-					BaseFont.IDENTITY_H, true);
+			
+			//FONT
+			BaseFont fontFamily = BaseFont.createFont("./font/times.ttf",BaseFont.IDENTITY_H, true);
 			Font blackColor = new Font(fontFamily, 12, 0, BaseColor.BLACK);
 			Font redColor = new Font(fontFamily, 12, 0, BaseColor.RED);
-			// LOOPING CARDS INTO DECK
+			
+			// PUT CARDS INTO DECK
 			char cardSign = ' ';
 			String cardValue = "";
 			Font color = null;
+			
 			// CARD SIGN
 			for (int sign = 0; sign < 4; sign++) {
 				switch (sign) {
-				case 0:
-					cardSign = '♠';
-					color = blackColor;
-					break;
-				case 1:
-					cardSign = '♥';
-					color = redColor;
-					break;
-				case 2:
-					cardSign = '♦';
-					color = redColor;
-					break;
-				case 3:
-					cardSign = '♣';
-					color = blackColor;
-					cardsBody.getDefaultCell().setBorderWidthBottom(0.5f);
-					break;
-
+					case 0: cardSign = '♠'; color = blackColor; break;
+					case 1: cardSign = '♥'; color = redColor; break;
+					case 2: cardSign = '♦'; color = redColor; break;
+					case 3: cardSign = '♣'; color = blackColor;
+						cardsBody.getDefaultCell()
+						.setBorderWidthBottom(0.5f); break;
 				}
+				
 				// CARD VALUE
 				for (int card = 2; card <= 14; card++) {
 					switch (card) {
-					case 11:
-						cardValue = "A";
-						break;
-					case 12:
-						cardValue = "J";
-						break;
-					case 13:
-						cardValue = "Q";
-						break;
-					case 14:
-						cardValue = "K";
-						cardsBody.getDefaultCell().setBorderWidthRight(0.5f);
-						break;
-					default:
-						cardValue = card + "";
-						cardsBody.getDefaultCell().setBorderWidthRight(0);
-						break;
+						case 11: cardValue = "A"; break;
+						case 12: cardValue = "J"; break;
+						case 13: cardValue = "Q"; break;
+						case 14: cardValue = "K";
+								cardsBody.getDefaultCell()
+								.setBorderWidthRight(0.5f); break;
+						default: cardValue = card + "";
+								cardsBody.getDefaultCell()
+								.setBorderWidthRight(0); break;
 					}
-					cardsBody.addCell(new Paragraph(cardValue + cardSign + "",
-							color));
+					cardsBody.addCell(new Paragraph(cardValue + cardSign + "",color));
 				}
 			}
 			// PRINT ALL CARDS AND CLOSE THE PDF
