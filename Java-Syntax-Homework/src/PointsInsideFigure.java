9a03ed44e912daa7cced6c245b5f4093204cecdc
Dimitar <mailto.dimitrov@gmail.com>
2015-01-19 02:00:04 +0200

    Update PointsInsideFigure.java

--- Java-Syntax-Homework/src/PointsInsideFigure.java
+++ Java-Syntax-Homework/src/PointsInsideFigure.java
@@ -6,6 +6,7 @@ public class PointsInsideFigure {
 		//LOCALE
 		Locale.setDefault(Locale.ROOT);
 		//SCANNER INPUT
+		@SuppressWarnings("resource")
 		Scanner scan = new Scanner(System.in);
 		
 		System.out.print("Type 2 points: ");
@@ -14,12 +15,15 @@ public class PointsInsideFigure {
 		//IS INSIDE OF THE LEFT PART OF THE FIGURE
 		boolean isInLeft = pointX >= 12.5 && pointX <= 17.5
 						&& pointY >= 8.5 && pointY <= 13.5,
+				
 		//IS INSIDE OF THE TOP PART OF THE FIGURE
 				isInTop = pointX >= 12.5 && pointX <= 22.5
 						&& pointY >= 6.0 && pointY <= 8.5,
+						
 		//IS INSIDE OF THE TOP PART OF THE FIGURE
 				isInRight = pointX >= 20.0 && pointX <= 22.5
 						&& pointY >= 8.5 && pointY <= 13.5;
+						
 						System.out.println(isInLeft || isInTop || isInRight ? "Inside" : "Outside");
 	}
 }
