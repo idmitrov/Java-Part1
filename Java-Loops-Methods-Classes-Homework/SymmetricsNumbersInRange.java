12cc9c1fb9d114649141e19c93a4e77944c901a0
idmitrov <idmitrov@gmail.com>
2015-01-30 20:04:17 +0200

    Improved to work with numbers greater than 999.

--- Java-Loops-Methods-Classes-Homework/SymmetricsNumbersInRange.java
+++ Java-Loops-Methods-Classes-Homework/SymmetricsNumbersInRange.java
@@ -6,65 +6,37 @@ public class SymmetricsNumbersInRange {
 		Scanner input = new Scanner(System.in);
 
 		int start = 0, end = 0;
-		 
 		do {
 			start = input.nextInt();
 			end = input.nextInt();
 		} while (start < 0 || start > end || end > 999);
-		 
-		int firstDigit = 0, lastDigit = 0;
 		
-		for (int i = start; i <= end; i++) {
-			if (i <= 9) {
-				System.out.printf("%d ", i);
-				continue;
-			}
+		for (int crntNum = start; crntNum <= end; crntNum++) { 
+			Boolean isSymmetric = true;
+			String tempNum = Integer.toString(crntNum);
 			
-			firstDigit = getFirstDigit(firstDigit, i);
-			lastDigit = i % 10;
-
-			if (firstDigit == lastDigit) {
-				System.out.printf("%d ", i);
+			for (int index = 0; index < (tempNum.length() + 1 ) / 2; index++) {
+				char leftSideDigit = tempNum.charAt(index),
+					 rightSideDigit = tempNum.charAt(tempNum.length() - index - 1);
+				if (leftSideDigit != rightSideDigit) {
+					isSymmetric = false;
+					break;
+				}
 			}
-			//speed up loop iterations
-			i = iterationBoost(i);
-		}
-	}
-
-	private static int getFirstDigit(int firstDigit, int i) {
-		if (i >= 10 && i <= 99) {
-			firstDigit = i / 10;
-		} else if (i >= 99 && i <= 999) {
-			firstDigit = i / 100;
-
-		}
-		return firstDigit;
-	}
-
-	private static int iterationBoost(int i) {
-		if (i >= 11 && i <= 88) {
-
-		} else if (i >= 101) {
-			if (i != 191 && i != 292 && i != 393 && i != 494 && i != 595
-					&& i != 696 && i != 797 && i != 898) {
-
-				i += 9;
-			} else {
-				i += 10;
+			if (isSymmetric) {
+				System.out.print(crntNum + " ");
 			}
-		}
-		return i;
+		}	
 	}
 }
 
-/* USING STING
-for (int i = start; i <= end; i++) {
-	StringBuilder iReversed = new
-	StringBuilder(Integer.toString(i)).reverse();
+/* USING STRING BUILDER
+for (int i = start; i <= end; i++) {i
+	StringBuilder iReversed = new StringBuilder(Integer.toString(i)).reverse();
 
 	if (iReversed.toString().equals(Integer.toString(i))) {
 		System.out.printf("%d ", i);
 	}
 }
-END OF USING STRING
+END
  */
